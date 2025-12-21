7a21c7ddad3fb0e82ded70e6da91a0a4aebae233
florinzgz <florinzgz@hotmail.com>
2025-12-21 23:48:55 +0100

    Fix misleading comments in SystemConfig.h - clarify unused parameters

--- src/core/SystemConfig.h
+++ src/core/SystemConfig.h
@@ -5,14 +5,12 @@
 #include "../../include/version.h"  // For FIRMWARE_VERSION
 #include "../../include/pins.h"     // For PIN_TFT_RST
 
-// Watchdog configuration
-// Timeout in milliseconds (note: actual watchdog uses 30 seconds internally)
-// This value is passed to Watchdog::init() but currently not used by the implementation
+// Watchdog timeout configuration (reserved for future use)
+// Note: Current implementation uses fixed 30-second hardware timeout internally
 #define WATCHDOG_TIMEOUT_MS 5000
 
-// Logging configuration
+// Logging level configuration (reserved for future use)
 // 0=DEBUG, 1=INFO, 2=WARNING, 3=ERROR
-// This value is passed to Logger::init() but currently not used by the implementation
 #define LOG_LEVEL 1
 
 // System timing
